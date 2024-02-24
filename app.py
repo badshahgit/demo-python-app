e8f296586b2159a173140360c4221b46107ed932
P321 <122960863+badshahgit@users.noreply.github.com>
2024-02-24 17:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'welcome to Elai Agritech pvt.ltd'
+    return 'Hello world'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
